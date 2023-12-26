a84d1d554359e5ebb922ffa5efdf2321bb9bbe88
TheClassybara <154922773+TheClassybara@users.noreply.github.com>
2023-12-26 17:51:10 -0600

    Messed with GUIs a bit more

--- caleb.py
+++ caleb.py
@@ -1,10 +1,12 @@
+"""
 import PySimpleGUI as psg
 layout = [
             [psg.Text(text='Hello World', font=('Quicksand', 20), size=20, expand_x=True, justification='center')],
             [psg.Checkbox(text = "This is a Checkbox", default = False)],
-            [psg.Button(button_text = "Exit", font = ("Quicksand", 20), button_color = "Blue")]
+            [psg.Button(button_text = "Exit", font = ("Quicksand", 20), button_color = "Black", justification = 'center')]
          ]
-window = psg.Window("Caleb's Test Window", layout)
+window = psg.Window("Caleb's Test Window", layout, resizable = True).Finalize()
+window.Maximize()
 while True:
    event, values = window.read()
    print(event, values)
@@ -13,6 +15,7 @@ while True:
    if event == "Close" or event == psg.WIN_CLOSED:
         break
 window.close()
+"""
 
 """
 import PySimpleGUI as sg
@@ -31,3 +34,28 @@ while True:
 
 window.close()
 """
+
+import PySimpleGUI as sg
+
+def main():
+    column_to_be_centered = [  [sg.Text('My Window')],
+                [sg.Input(key='-IN-')],
+                [sg.Text(size=(12,1), key='-OUT-')],
+                [sg.Button('Go'), sg.Button('Exit')]]
+
+    layout = [[sg.VPush()],
+              [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
+              [sg.VPush()]]
+
+    window = sg.Window('Window Title', layout, size=(500,300))
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+
+    window.close()
+
+
+if __name__ == '__main__':
+    main()
