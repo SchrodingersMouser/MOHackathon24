aeb8b8684e15910e6eb641852e55d497c3031824
TheClassybara <154922773+TheClassybara@users.noreply.github.com>
2023-12-26 17:28:15 -0600

    I experimented with some PySimpleGUI stuff

--- caleb.py
+++ caleb.py
@@ -1,14 +1,33 @@
 import PySimpleGUI as psg
-layout = [[psg.Text(text='Hello World',
-   font=('Arial Bold', 20),
-   size=20,
-   expand_x=True,
-   justification='center')],
-]
-window = psg.Window('HelloWorld', layout, size=(715,250))
+layout = [
+            [psg.Text(text='Hello World', font=('Quicksand', 20), size=20, expand_x=True, justification='center')],
+            [psg.Checkbox(text = "This is a Checkbox", default = False)],
+            [psg.Button(button_text = "Exit", font = ("Quicksand", 20), button_color = "Blue")]
+         ]
+window = psg.Window("Caleb's Test Window", layout)
 while True:
    event, values = window.read()
    print(event, values)
    if event in (None, 'Exit'):
-      break
+        break
+   if event == "Close" or event == psg.WIN_CLOSED:
+        break
 window.close()
+
+"""
+import PySimpleGUI as sg
+
+layout = [  [sg.Text('My Window'), sg.Push(), sg.Button('Upper Right')],
+            [sg.Input(key='-IN-')],
+            [sg.Button('Exit'), sg.Push(), sg.Button('Lower Right')]  ]
+
+window = sg.Window('Window Title', layout)
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+window.close()
+"""
