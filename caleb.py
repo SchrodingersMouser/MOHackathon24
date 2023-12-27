d05ac65790533cf73404eab7112bc2f654e6bcd9
TheClassybara <154922773+TheClassybara@users.noreply.github.com>
2023-12-26 18:45:23 -0600

    Commiting again because of issues

--- caleb.py
+++ caleb.py
@@ -1,21 +1,21 @@
-"""
+
 import PySimpleGUI as psg
 layout = [
             [psg.Text(text='Hello World', font=('Quicksand', 20), size=20, expand_x=True, justification='center')],
             [psg.Checkbox(text = "This is a Checkbox", default = False)],
             [psg.Button(button_text = "Exit", font = ("Quicksand", 20), button_color = "Black", justification = 'center')]
-         ]
+        ]
 window = psg.Window("Caleb's Test Window", layout, resizable = True).Finalize()
 window.Maximize()
 while True:
-   event, values = window.read()
-   print(event, values)
-   if event in (None, 'Exit'):
+    event, values = window.read()
+    print(event, values)
+    if event in (None, 'Exit'):
         break
-   if event == "Close" or event == psg.WIN_CLOSED:
+    if event == "Close" or event == psg.WIN_CLOSED:
         break
-window.close()
-"""
+    window.close()
+
 
 """
 import PySimpleGUI as sg
@@ -35,27 +35,27 @@ while True:
 window.close()
 """
 
-import PySimpleGUI as sg
+# import PySimpleGUI as sg
 
-def main():
-    column_to_be_centered = [  [sg.Text('My Window')],
-                [sg.Input(key='-IN-')],
-                [sg.Text(size=(12,1), key='-OUT-')],
-                [sg.Button('Go'), sg.Button('Exit')]]
+# def main():
+#     column_to_be_centered = [  [sg.Text('My Window')],
+#                 [sg.Input(key='-IN-')],
+#                 [sg.Text(size=(12,1), key='-OUT-')],
+#                 [sg.Button('Go'), sg.Button('Exit')]]
 
-    layout = [[sg.VPush()],
-              [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
-              [sg.VPush()]]
+#     layout = [[sg.VPush()],
+#               [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
+#               [sg.VPush()]]
 
-    window = sg.Window('Window Title', layout, size=(500,300))
+#     window = sg.Window('Window Title', layout, size=(500,300))
 
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Exit':
-            break
+#     while True:
+#         event, values = window.read()
+#         if event == sg.WIN_CLOSED or event == 'Exit':
+#             break
 
-    window.close()
+#     window.close()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
