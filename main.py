520f8f15400934a0c84ab897f7dcf726ca97e8a7
Bertram <113191739+SchrodingersMouser@users.noreply.github.com>
2024-02-03 13:40:32 -0600

    fleshing out logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+
 pygame.init()
 
 WINDOW_HEIGHT = 1000
@@ -8,16 +9,29 @@ WINDOW_WIDTH = 1000
 screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
 
 clock = pygame.time.Clock()
+def ship():
+    s_location = (300, 500) # ship location
+    s_velocity = (-40, 300) # ship speed (0 - 100) and direction (0 - 360)
+    ship_icon = pygame.image.load("images/ship.png").convert() # converting the image makes it run faster (supposedly)
+def update_ship_velocity():
+    #this function should modify the ship's velocity based on user inputs
+def update_ship_location():
+    #this function should modify the ship's location
+    # ship orientation should be set based on the ship heading
+
 
 while True:
+
     # Process player inputs.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
 
-    # Do logical updates here.
-    # ...
+    #logic updates
+    update_ship_velocity()
+
+    update_ship_location()
 
     # TODO: Try to make this into an image
     screen.fill("black")  # Fill the display with a solid color
@@ -29,3 +43,4 @@ while True:
 
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(60)         # wait until next frame (at 60 FPS)
+    
